a62cd1c6d303e0e9a7cb477ecbb73f2e8dbd5879
Aktilek <janybaevaktilek@gmail.com>
2022-10-25 14:47:50 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import "./App.css";
+import Block from "./components/Block";
+
+const blocks = [
+  {
+    id: "b2",
+    block: "green",
+  },
+  {
+    id: "b3",
+    block: "color",
+  },
+  {
+    id: "b4",
+    block: `${Math.random()}`,
+  },
+  {
+    id: "b5",
+    block: "green",
+  },
+  {
+    id: "b6",
+    block: `${Math.random()}`,
+  },
+  {
+    id: "b1",
+    block: "yellow",
+  },
+];
+
+
+function App() {
+  return (
+    <div className="App">
+      {blocks.map((item) =>
+        item.block === "yellow" ? (
+          <Block className="yellow" key={item.id} />
+        ) : item.block === "green" ? (
+          <Block className="green" key={item.id} />
+        ) : item.block === "color" ? (
+          <Block className="lime" key={item.id} />
+        ) : (
+          <Block className="red" key={item.id} />
+        )
+      )}
+    </div>
+  );
+}
+
+export default App;
